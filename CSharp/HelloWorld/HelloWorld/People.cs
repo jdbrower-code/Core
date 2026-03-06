108ad807f0da6e7cb96bdb3311b3747315431d15
Brower_Jason <jbrower@sanmarcostx.gov>
2026-03-06 10:37:18 -0600

    csharp learing push 1

---
+++ CSharp/HelloWorld/HelloWorld/People.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People
+{
+    internal class people
+    {
+        private string Name;
+        private string Age;
+        private string Height;
+        private string Weight;
+
+        public void setName()
+        {
+            Console.WriteLine("Please Type your Name then hit the Enter Key");
+            Name = Console.ReadLine();
+
+
+        }
+
+        public void setAge()
+        {
+            Console.WriteLine("Please Type your Age then hit the Enter Key");
+            Age = Console.ReadLine();
+        }
+
+        public void setHeight()
+        {
+            Console.WriteLine("Please Type your Height then hit the Enter Key");
+            Height = Console.ReadLine();
+        }
+
+        public void setWeight()
+        {
+            Console.WriteLine("Please Type your Weight then hit the Enter Key");
+            Weight = Console.ReadLine();
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+
+        public string getAge()
+        {
+            return Age;
+        }
+
+        public string getHeight()
+        {
+            return Height;
+        }
+
+        public string getWeight()
+        {
+            return Weight;
+        }
+    }
+}
+
+
---
+++ CSharp/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using People;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            people person = new people();
+            person.setName();
+            person.setAge();
+            person.setHeight();
+            person.setWeight();
+            Console.WriteLine("Name: " + person.getName());
+            Console.WriteLine("Age: " + person.getAge());
+            Console.WriteLine("Height: " + person.getHeight());
+            Console.WriteLine("Weight: " + person.getWeight());
+        }
+    }
+}
